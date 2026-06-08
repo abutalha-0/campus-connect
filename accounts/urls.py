80d6f0a99654421509ef6bb7539bcf4f042b7bd5
Abu Talha <atalha.ab@gmail.com>
2026-06-08 08:26:46 +0600

    feat: add accounts URLs

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (
+    RegisterView,
+    LoginView,
+    ProfileView,
+    UserListView,
+    UserDetailView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+]
